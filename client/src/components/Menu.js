6ce7f3b13163dffbd0f3264279cdf2de7429d0cf
William (Will) Turner <99505215+WillTurner-ST@users.noreply.github.com>
2024-01-26 16:35:41 -0500

    Update Menu.js

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -6,7 +6,7 @@ const Menu = () => {
 
   return (
 <div className="text-center mb-20 mt-20">
-    <h2 className="text-white text-5xl mb-10">Menu</h2>
+    <h2 className="text-white text-4xl mb-10">Menu</h2>
     <div className="flex flex-col md:flex-row justify-center items-center">
         <div className="md:w-1/4 mx-5 mb-5 md:mb-0 max-w-xs md:max-w-sm lg:max-w-md">
             <img src={MenuL} alt="Menu 1" className="w-full" />
