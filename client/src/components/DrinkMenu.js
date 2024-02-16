430b5a28d6e9a9b1cb6aca093057773f5d75a9fb
WillTurnerST <will.t@snapdragonhemp.com>
2024-02-16 11:42:31 -0500

    Added food menu

--- client/src/components/DrinkMenu.js
+++ client/src/components/DrinkMenu.js
@@ -3,13 +3,22 @@ import DabL from '../assets/DabL.png'
 import DabR from '../assets/DabR.png'
 import MenuL from '../assets/MenuL.png'
 import MenuR from '../assets/MenuR.png'
+import FoodMenu from '../assets/FoodMenu.png'
 
 const DrinkMenu = () => {
 
   return (
     <div className="container mx-auto p-4">
+              {/* Food Menu Heading and Images */}
+              <h2 className="text-4xl text-white text-center mb-4">Food Menu</h2>
+      <div className="items-center">
+        <div className="max-w-md mx-auto">
+          <img src={FoodMenu} alt="Drink Menu Left" className="w-full" />
+        </div>
+      </div>
+
       {/* Drink Menu Heading and Images */}
-      <h2 className="text-4xl text-white text-center mb-4">Drink Menu</h2>
+      <h2 className="text-4xl text-white text-center mb-4 mt-10">Drink Menu</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8 items-center">
         <div className="max-w-md mx-auto">
           <img src={MenuL} alt="Drink Menu Left" className="w-full" />
